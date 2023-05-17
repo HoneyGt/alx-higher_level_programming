db23500bed9e7aeb08f2817cfd962c11fb06009d
Honey Gt <nadhigutu17@gmail.com>
2023-05-17 12:32:39 -0700

    Task 13

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -5,7 +5,7 @@ def weight_average(my_list=[]):
         return 0
     num = 0
     den = 0
-    for tiup in my_list:
+    for tup in my_list:
         num += tup[0] * tup[1]
         den += tup[1]
     return (num / den)
