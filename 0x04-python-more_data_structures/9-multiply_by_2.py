fa6619b83a24359150fe83691070d563168b830c
Honey Gt <nadhigutu17@gmail.com>
2023-05-17 10:44:59 -0700

    Task 9

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def multiply_by_2(a_dictionary):
+    new_dir = a_dictionary.copy()
+    list_keys = list(new_dir.keys())
+    for i in list_key:
+        new_dir[i] *= 2
+    return (new_dir)
