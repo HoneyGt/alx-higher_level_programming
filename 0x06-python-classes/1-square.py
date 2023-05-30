068529de0c49becef535598f34163599b0814d41
HoneyGt <nadhigutu@gmail.com>
2023-05-30 11:18:05 -0700

    Task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -8,6 +8,6 @@ class Square:
 def __init__(self, size):
     """Initialize a new Square.
     Args:
-        size (int): The size of the new square.
+        Size (int): The size of the new square.
         """
         self.__size = size
