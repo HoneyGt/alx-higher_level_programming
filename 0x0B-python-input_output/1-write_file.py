1ffb0e1c01fded39866ad56d029e8a020809b74a
HoneyGt <nadhigutu@gmail.com>
2023-06-13 11:28:28 -0700

    Task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,6 +4,7 @@
 
 def write_file(filename="", text=""):
     """Write a string to UTF8  a text file.
+
     Args:
         filename (str): The name of the file to write.
         text (str): The text to write to the file.
