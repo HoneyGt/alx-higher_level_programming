c5d48db5aadb8b62dd01da12a1116e1b0675aa4a
Honey Gt <nadhigutu17@gmail.com>
2023-08-16 12:38:01 -0700

    Concat files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -4,4 +4,4 @@ const fs = require('fs');
 const i = fs.readFileSync(process.argv[2], 'utf8');
 const j = fs.readFileSync(process.argv[3], 'utf8');
 
-fs.writeFileSync(process.argv[4]), i + j);
+fs.writeFileSync(process.argv[4], i + j);
